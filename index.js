d6973ad2666fd2bc9aa0e35e9eb4a28e2213067b
Subham Mani <manisubham09@gmail.com>
2023-07-27 10:45:09 +0530

    structure code

--- index.js
+++ index.js
@@ -3,33 +3,14 @@ const cpwdField = document.querySelector('.cpwd_field');
 const cpwdInfo = document.querySelector('.cpwd-info');
 
 cpwdField.addEventListener('input', () => {
-    if(pwdField.value !== ''){
-        if(cpwdField.value === pwdField.value){
-            // valid
-
-            cpwdInfo.classList.remove('invalid');
-            cpwdInfo.classList.add('valid');
-
-            cpwdField.setCustomValidity('');
-        }
-        else{
-            //invalid
-
-            cpwdInfo.classList.add('invalid');
-            cpwdInfo.classList.remove('valid');
-
-            cpwdField.setCustomValidity("Password doesn't match");
-        }
-    }
-    else{
-        //invalid
-
-        cpwdInfo.classList.add('invalid');
-        cpwdInfo.classList.remove('valid');
-    }
+    validatePassword();
 });
 
 pwdField.addEventListener('input', () => {
+    validatePassword();
+});
+
+function validatePassword(){
     if(pwdField.value !== ''){
         if(cpwdField.value === pwdField.value){
             // valid
@@ -54,4 +35,4 @@ pwdField.addEventListener('input', () => {
         cpwdInfo.classList.add('invalid');
         cpwdInfo.classList.remove('valid');
     }
-});
+}
